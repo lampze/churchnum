7dd762ce79db702a0dc94b624be47371a3a80d30
shirui <1229408499@qq.com>
2020-08-14 10:28:15 +0800

    Add basic num for js

---
+++ churchnum.js
@@ -0,0 +1,15 @@
+let zero = (f) => (x) => x;
+let succ = (n) => (f) => (x) => f(n(f)(x));
+let plusv1 = (n) => (m) => (f) => (x) => n(f)(m(f)(x));
+let plusv2 = (n) => (m) => n(succ)(m);
+let multv1 = (n) => (m) => m(plus(n))(zero);
+let multv2 = (n) => (m) => (f) => m(n(f));
+let exp = (n) => (m) => n(m);
+let plus = plusv1;
+let mult = multv2;
+let one = (f) => (x) => f(x);
+let two = succ(one);
+let three = succ(two);
+let four = succ(three);
+let add1 = (n) => n + 1;
+let prt = (x) => x(add1)(0);
